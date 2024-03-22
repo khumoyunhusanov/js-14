1d01ac9839e94ce56d5fd9f741a7a1b5b44aa964
khumoyunhusanov <rashidovalutfiya@gmail.com>
2024-03-22 10:20:15 +0500

    homework 14

---
+++ main.js
@@ -0,0 +1,94 @@
+// alert("salom")
+
+
+    let arr = [
+        {
+            "userId": 1,
+            "id": 1,
+            "title": "delectus aut au`tem",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 2,
+            "title": "quis ut nam facilis et officia qui",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 3,
+            "title": "fugiat veniam minus",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 4,
+            "title": "et porro tempora",
+  
+            "completed": true
+
+        },
+        {
+            "userId": 1,
+            "id": 5,
+            "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 6,
+            "title": "qui ullam ratione quibusdam voluptatem quia omnis",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 7,
+            "title": "illo expedita consequatur quia in",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 8,
+            "title": "quo adipisci enim quam ut ab",
+            "completed": true
+        },
+        {
+            "userId": 1,
+   
+            "id": 9,
+            "title": "molestiae perspiciatis ipsa",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 10,
+            "title": "illo est ratione doloremque quia maiores aut",
+            "completed": true
+        },
+    ]
+    let trues = {
+        count: 0,
+        arr: []
+    }
+   
+    let falses = {
+        count: 0,
+        arr: []
+    }
+
+
+   
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i].completed == true) {
+              trues.count++,
+              trues.arr.push(arr[i].completed)
+            }else{
+              falses.count++,
+              falses.arr.push(arr[i].completed)
+            }
+      }
+
+    console.log(trues);
+    console.log(falses);
+
+
